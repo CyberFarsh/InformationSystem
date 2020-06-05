4f481b80d74b4cddc33b8844d3b6926efe68bd10
Denis Ryndin <ryndin.denis@mail.ru>
2020-06-06 00:17:09 +0300

    Add home page and navbar

--- InformationSystem/InformationSystem/settings.py
+++ InformationSystem/InformationSystem/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
+    'Altai.apps.AltaiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
