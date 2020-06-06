5d76164a8ca24b1858f30d26f649c008e0cf1c6d
Denis Ryndin <ryndin.denis@mail.ru>
2020-06-06 15:38:59 +0300

    Add loggout link and new special_func redirect

--- InformationSystem/Altai/urls.py
+++ InformationSystem/Altai/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', views.home, name='home-page'),
+    path('logout/', views.logout, name='logout'),
     path('', include('social_django.urls'))
 ]
--- InformationSystem/Altai/views.py
+++ InformationSystem/Altai/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout as auth
+from .special_func import get_next_url
 
 
 def home(request):
     return render(request, 'Altai/home.html')
+
+
+@login_required
+def logout(request):
+    auth.logout(request)
+    return redirect(get_next_url(request))
